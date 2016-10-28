5ed4edab191c047d30582f752fd89da0f37341a0
Naris R <nariscatboy@gmail.com>
2016-10-28 16:28:54 +1100

    Redescribed the interface methods

--- PrefixMap.java
+++ PrefixMap.java
@@ -13,7 +13,7 @@ public interface PrefixMap {
 	
 	/*
 	 * Get the value corresponding to the key (or null if the key is not found)
-	 * if the key contains any character other than A,C,G,T, throw MalformedKeyException
+	 * if the key contains any non-valid character, throw MalformedKeyException
 	 * if the key is null, throw IllegalArgumentException
 	 */
 	public String get(String key);
@@ -21,7 +21,7 @@ public interface PrefixMap {
 	/*
 	 * Insert the value into the data structure, using the given key. If the key
 	 * already existed, replace and return the old value (otherwise return null)
-	 * if the key contains any character other than A,C,G,T, throw MalformedKeyException
+	 * if the key contains any non-valid character, throw MalformedKeyException
 	 * if the key or value is null, throw IllegalArgumentException
 	 */
 	public String put(String key, String value);
@@ -29,21 +29,21 @@ public interface PrefixMap {
 	/*
 	 * Remove the value corresponding to the given key from the data structure,
 	 * if it exists. Return the old value, or null if no value was found.
-	 * if the key contains any character other than A,C,G,T, throw MalformedKeyException
+	 * if the key contains any non-valid character, throw MalformedKeyException
 	 * if the key is null, throw IllegalArgumentException
 	 */
 	public String remove(String key);
 
 	/*
 	 * return the number of keys which start with the given prefix if the prefix
-	 * contains any character other than A,C,G,T, throw MalformedKeyException
+	 * contains any non-valid character, throw MalformedKeyException
 	 * if the prefix is null, throw IllegalArgumentException
 	 */
 	public int countKeysMatchingPrefix(String prefix);
 
 	/*
 	 * return the collection of keys which start with the given prefix if the
-	 * prefix contains any character other than A,C,G,T, throw MalformedKeyException
+	 * prefix contains any non-valid character, throw MalformedKeyException
 	 * if the prefix is null, throw IllegalArgumentException
 	 */
 	public List<String> getKeysMatchingPrefix(String prefix);
